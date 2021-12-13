66566391bff45b2da072082dcc0dd75e76d4f37d
Mwapsam <mwapsam@gmail.com>
2021-12-13 17:02:20 +0200

    fix linter errors

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
-let menuBtn = document.getElementById("menu-icon");
-let navElement = document.getElementById("mobile-menu");
+const menu = document.querySelector('.menu');
+const menuItems = document.querySelectorAll('.menuItem');
+const hamburger = document.querySelector('.hamburger');
+const closeIcon = document.querySelector('.closeIcon');
+const menuIcon = document.querySelector('.menuIcon');
 
-menuBtn.addEventListener("click", ()=>{
-    if (menuBtn.classList.contains("menu")) {
-    menuBtn.classList.remove("menu");
-    menuBtn.classList.add("close");
-    navElement.style.display = "none";
-  } else if (menuBtn.classList.contains("close")) {
-    menuBtn.classList.add("menu");
-    menuBtn.classList.remove("close");
-    navElement.style.display = "block";
+function toggleMenu() {
+  if (menu.classList.contains('showMenu')) {
+    menu.classList.remove('showMenu');
+    closeIcon.style.display = 'none';
+    menuIcon.style.display = 'block';
+  } else {
+    menu.classList.add('showMenu');
+    closeIcon.style.display = 'block';
+    menuIcon.style.display = 'none';
   }
-});
+}
 
+hamburger.addEventListener('click', toggleMenu);
+
+menuItems.forEach((menuItem) => {
+  menuItem.addEventListener('click', toggleMenu);
+});
