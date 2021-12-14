e219e621957c90061e0ec8209d11df9d6124f876
Mwapsam <mwapsam@gmail.com>
2021-12-14 14:33:13 +0200

    Changes on index.html

--- main.js
+++ main.js
@@ -21,3 +21,22 @@ hamburger.addEventListener('click', toggleMenu);
 menuItems.forEach((menuItem) => {
   menuItem.addEventListener('click', toggleMenu);
 });
+
+// the modal
+const modal = document.getElementById('myModal');
+
+const btn = document.getElementById('myBtn');
+const span = document.getElementsByClassName('close')[0];
+btn.onclick = function () {
+  modal.style.display = 'block';
+};
+
+span.onclick = function () {
+  modal.style.display = 'none';
+};
+
+window.onclick = function (event) {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+};
