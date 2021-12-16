c4500bfc4491c05593a3eed534daf34827a245e4
Mwapsam <mwapsam@gmail.com>
2021-12-16 15:07:59 +0200

    Add local storages to the form

--- main.js
+++ main.js
@@ -209,3 +209,31 @@ document.getElementById('submit').addEventListener('click', (e) => {
     e.preventDefault();
   }
 });
+
+// Local-Storage
+const clientName = document.getElementById('fname');
+const clientEmail = document.getElementById('email');
+const clientMessage = document.getElementById('textarea');
+
+function useLocalStorage() {
+  const contactFormData = {
+    name: clientName.value,
+    email: clientEmail.value,
+    message: clientMessage.value,
+  };
+  localStorage.setItem('contactFormData', JSON.stringify(contactFormData));
+}
+
+const formInputs = document.querySelectorAll('input , textarea');
+for (let j = 0; j < formInputs.length; j += 1) {
+  formInputs[j].addEventListener('change', () => {
+    useLocalStorage();
+  });
+}
+
+const formData = JSON.parse(localStorage.getItem('contactFormData'));
+if (formData !== null) {
+  clientName.value = formData.name;
+  clientEmail.value = formData.email;
+  clientMessage.value = formData.message;
+}
